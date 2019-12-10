e0886109a8e030293d173ff02302b677e1c7adbb
Austin Dean <austin@atdean.com>
2019-12-10 00:52:51 -0500

    Refactored app to initialize router and act as an http.Handler.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,12 +5,10 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/atdean/onomatopoedia/pkg/webserver"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gomodule/redigo/redis"
 	"github.com/jmoiron/sqlx"
-	"github.com/julienschmidt/httprouter"
-
-	"github.com/atdean/onomatopoedia/pkg/webserver"
 )
 
 func main() {
@@ -24,28 +22,8 @@ func main() {
 		log.Fatalln(err);
 	}
 
-	//app := webserver.App{
-	//	SqlPool: sqlPool,
-	//	RedisConn: &redisConn,
-	//	IndexController: webserver.NewIndexController(sqlPool),
-	//	AuthController: webserver.NewAuthController(sqlPool, &redisConn),
-	//}
-
 	app := webserver.InitApp(sqlPool, &redisConn)
 
 	fmt.Println("HTTP server started... listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", initRoutes(app)))
-}
-
-func initRoutes(app *webserver.App) http.Handler {
-	router := httprouter.New()
-
-	router.GET("/", app.IndexController.GetIndexHandler)
-
-	router.GET("/login", app.AuthController.GetLoginHandler)
-	router.POST("/login", app.AuthController.PostLoginHandler)
-
-	router.ServeFiles("/static/*filepath", http.Dir("./public"))
-
-	return router
+	log.Fatal(http.ListenAndServe(":8080", app))
 }
--- pkg/webserver/app.go
+++ pkg/webserver/app.go
@@ -3,12 +3,15 @@ package webserver
 import (
 	"github.com/gomodule/redigo/redis"
 	"github.com/jmoiron/sqlx"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
 // App is the central dependency for all route controllers and config
 type App struct {
 	SqlPool         *sqlx.DB
 	RedisConn 		*redis.Conn
+	Router			http.Handler
 	IndexController *IndexController
 	AuthController 	*AuthController
 }
@@ -22,5 +25,25 @@ func InitApp(sqlPool *sqlx.DB, redisConn *redis.Conn) *App {
 	app.IndexController = NewIndexController(app)
 	app.AuthController = NewAuthController(app)
 
+	app.Router = app.initRoutes()
+
 	return app
 }
+
+func (app *App) initRoutes() http.Handler {
+	router := httprouter.New()
+
+	router.GET("/", app.IndexController.GetIndexHandler)
+
+	router.GET("/login", app.AuthController.GetLoginHandler)
+	router.POST("/login", app.AuthController.PostLoginHandler)
+
+	router.ServeFiles("/static/*filepath", http.Dir("./public"))
+
+	return router
+}
+
+func (app *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	app.Router.ServeHTTP(w, r)
+}
+
